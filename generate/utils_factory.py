030cba1a40b531095fd601c93ed7d292cec8b738
Mark A. Greenslade <mark@casperlabs.io>
2021-04-07 14:20:49 +0200

    1. Ensured project derivation is generic.

--- generate/utils_factory.py
+++ generate/utils_factory.py
@@ -155,7 +155,7 @@ def _set_topic_from_module(topic, parent):
         topic.change_history = topic.spec.CHANGE_HISTORY
         topic.contributors = topic.spec.CONTRIBUTORS
         topic.name = topic.spec.__name__
-        topic.id = "cordex.{}".format(topic.name)
+        topic.id = "{}.{}".format(topic.spec.__file__.split("/")[-2], topic.name)
 
     # Assign properties / property sets.
     if hasattr(topic.spec, "DETAILS") and hasattr(topic.spec, "ENUMERATIONS"):
